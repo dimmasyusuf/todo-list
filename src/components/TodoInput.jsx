7f4585e5fc796b30e57250b4ddc574b6a6fae045
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-06 15:00:02 +0700

    feat(todoinput): use formik and yup

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,7 +1,44 @@
-import { Box, IconButton, FormControl, Input, HStack } from '@chakra-ui/react';
+import {
+  Box,
+  IconButton,
+  FormControl,
+  Input,
+  HStack,
+  FormErrorMessage,
+} from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useState } from 'react';
 
 function TodoInput() {
+  const [loadingState, setLoadingState] = useState(false);
+
+  const todoSchema = Yup.object().shape({
+    todo: Yup.string()
+      .required('Todo is a required field')
+      .min(3, 'Todo must be at least 3 characters')
+      .max(140, 'Todo must be less than 140 characters'),
+  });
+
+  const addTodo = () => {
+    setLoadingState(true);
+    console.log(formik.values);
+  };
+
+  const handleValue = (event) => {
+    const { target } = event;
+    formik.setFieldValue(target.name, target.value);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      todo: '',
+    },
+    onSubmit: addTodo,
+    validationSchema: todoSchema,
+  });
+
   return (
     <Box
       borderWidth='1px'
@@ -10,12 +47,23 @@ function TodoInput() {
       borderRadius='lg'
       width='70%'
     >
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <HStack>
-          <FormControl>
-            <Input type='text' placeholder='What are you going to do?' />
+          <FormControl isInvalid={formik.errors.todo && formik.touched.todo}>
+            <Input
+              type='text'
+              placeholder='What are you going to do?'
+              onChange={handleValue}
+              name='todo'
+            />
+            <FormErrorMessage>{formik.errors.todo}</FormErrorMessage>
           </FormControl>
-          <IconButton colorScheme='teal' icon={<AddIcon />} />
+          <IconButton
+            colorScheme='teal'
+            icon={<AddIcon />}
+            isLoading={loadingState}
+            type='submit'
+          />
         </HStack>
       </form>
     </Box>
